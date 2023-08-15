e9c57ecfa7748411647f39e881a0de50189cb11e
anvitajohri <anvitajohri@192.168.0.100>
2023-08-16 01:38:52 +0300

    Updating main branch

--- Weather/src/main/java/com/weather/exceptions/WeatherNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.weather.exceptions;
-
-@SuppressWarnings("serial")
-public class WeatherNotFoundException extends RuntimeException {
-
-    public WeatherNotFoundException(String message) {
-        super(message);
-    }
-}
